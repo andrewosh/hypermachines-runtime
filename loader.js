e59319328633b3e38e7590b44324bc207fd49535
Andrew Osheroff <andrewosh@gmail.com>
2020-10-20 14:26:49 +0200

    0.0.7

---
+++ loader.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const p = require('path')
+const os = require('os')
+
+const PLATFORM = os.platform()
+const FILENAME = `hypermachines-runtime.${PLATFORM}.node`
+const LOCAL_BINDINGS = p.join(__dirname, FILENAME)
+const DEP_BINDINGS = p.join(__dirname, 'node_modules', '@hypermachines', `runtime-${PLATFORM}`, FILENAME)
+
+module.exports = function loadBindings () {
+  try {
+    fs.statSync(LOCAL_BINDINGS)
+    return require(LOCAL_BINDINGS)
+  } catch (_) {
+    return require(DEP_BINDINGS)
+  }
+}
